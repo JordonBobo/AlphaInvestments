4e2ec01cbc235ce5eaf8f427fdef98d5e9e80f67
Isaac Richardson <isaac.m.richardson16@gmail.com>
2021-01-23 12:33:53 -0700

    added ajax calls and search bar functionality, mob coded

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-var hi = 1;
+$("#searchBtn").click(function () {
+  var company = $("#companySearch").val();
+  console.log(company);
+  var overviewUrl =
+    "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
+    company +
+    "&apikey=HJSI78XPL8KLZ1JA";
+
+  $.ajax({
+    url: overviewUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+
+  var stockUrl =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" +
+    company +
+    "&apikey=HJSI78XPL8KLZ1JA";
+
+  $.ajax({
+    url: stockUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    for (let [data] of Object.entries(response["Weekly Time Series"])) {
+      console.log(data);
+    }
+  });
+});

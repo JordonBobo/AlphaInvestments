bf1144c3a5b0d334542821931ac711eb97db3271
Jordon Bobo <jordon.bobo@outlook.com>
2021-01-28 20:32:04 -0700

    refurbished code and organized everything

--- script.js
+++ script.js
@@ -1,184 +1,41 @@
-//    add as univerisal variables in JS ================================
-var closingValue1 = [];
-var stock1 = "";
-var closingValue2 = [];
-var stock2 = "";
-var closingDate = [];
-var newsCompany = "";
 
-//    add as univerisal variables in JS ================================
 
+  //Universal variables and placeholders that need to be unversal scope
+var weeklyValue = [];
+var weeklyDate = [];
+var vantageKey = "PPU2C3YN8OQAFVPV";        //stock api key
+var newsApiKey = "mucdpZxSaLprDSOXjHQmG9skw5jyQeci";
+var currentCompany = "";
+var currentSymbol = "";
+
+
+
+            //==================PART I === Search Bar ==============\\
 $("#searchBtn2").click(function () {
-  var vantageKey = "PPU2C3YN8OQAFVPV";
-  var company = $("#companySearch2").val();
-  console.log(company);
+  var tempCompany = $("#companySearch2").val();   //form to get user search input
 
-  // searching company name and gathering stock abbreviation
-
-  var symbolSearch =
+                      // URL that searches best matches for possible company symbol
+  var getSymbol =
     "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" +
-    company +
+    tempCompany +
     "&apikey=" +
     vantageKey;
 
+      // API Call that gets the symbol from the user and esablishes the currentCompany
   $.ajax({
-    url: symbolSearch,
+    url: getSymbol,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-
-    company = selectOneStock(response.bestMatches);
-  });
-
-  // COMPANY OVERVIEW API CALL
-
-  var overviewUrl =
-    "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
-    company +
-    "&apikey=" +
-    vantageKey;
-
-  $.ajax({
-    url: overviewUrl,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-
-    // NEWS SEARCH API CALL
-    var newsCompany = response.Name;
-
-    var newsUrl =
-      "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-      newsCompany +
-      "&api-key=mucdpZxSaLprDSOXjHQmG9skw5jyQeci";
-
-    $.ajax({
-      url: newsUrl,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response.response.docs[0]);
-    });
-  });
-
-  // STOCK DATA API CALL
-
-  var stockUrl =
-    "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" +
-    company +
-    "&apikey=" +
-    vantageKey;
-
-  $.ajax({
-    url: stockUrl,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-
-    //  MAKE SURE TO CLEAR THE DATA ONCLICK EVENT SO IT DOESN"T TACK THEM ON !!!!!!
-    // My stuff to add =============================================================
-    //source: chartjs.org
-    stock1 = response["Meta Data"][Symbol];
-
-    for (let i = 0; i < 52; i++) {
-      var priceDate = Object.keys(response["Weekly Time Series"])[i];
-      // console.log(response["Weekly Time Series"][priceDate]["4. close"]);
-      closingValue1.unshift(
-        response["Weekly Time Series"][priceDate]["4. close"]
-      );
-      closingDate.unshift(priceDate);
-    }
-
-    visualData();
-    function visualData() {
-      var ctx = document.getElementById("chart").getContext("2d");
-      var myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-          labels: closingDate, //what is listed as the x axis value
-          datasets: [
-            {
-              label: stock1,
-              data: closingValue1,
-              backgroundColor: "rgba(0, 0, 0, 1)",
-              borderColor: "rgba(0, 0, 0, 1)",
-              borderWidth: 3,
-              fill: false,
-            },
-            {
-              label: stock2,
-              data: closingValue2,
-              backgroundColor: "rgba(150, 150, 150, 1)",
-              borderColor: "rgba(150, 150, 150, 1)",
-              borderWidth: 3,
-              fill: false,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  // beginAtZero: true
-                  callback: function (value, index, values) {
-                    return "$" + value;
-                  },
-                },
-              },
-            ],
-          },
-        },
-      });
-    }
-
-    //    Local storage stuff
-    var stockSymbol = "tsla";
-    localStorage.setItem("key1", stockSymbol);
-
-    var storedInfo = localStorage.getItem("key1");
-    function displayLastSearch() {
-      if (storedInfo !== undefined && storedInfo !== null) {
-        stockSymbol = storedInfo;
-      }
-    }
-
-    $("#searchBtn2").click(addsecond());
-
-    function addsecond() {
-      for (let i = 0; i < 52; i++) {
-        closingValue2.unshift(i * 20);
-      }
-      visualData();
-    }
-
-    // End of my stuff to add =============================================================
-
-    for (let data of Object.entries(response["Weekly Time Series"])) {
-      // console.log(data);
-    }
-  });
-});
-
-// Other data
-
-// These don't yet exist on the page, but we can add easily
-// High and low price in the last year
-$("#high").text(high);
-var testArray = [1, 6, 8, 9, 9];
-var high =
-  "$" +
-  testArray.reduce(function (a, b) {
-    return Math.max(a, b);
-  });
-var low =
-  "$" +
-  testArray.reduce(function (a, b) {
-    return Math.min(a, b);
-  });
+    currentCompany = selectOneStock(response.bestMatches);
+    
+    console.log(currentCompany)
+          // function for deciding which stock the user wants
 function selectOneStock(arrBestMatches) {
   $("#stockList").empty();
   if (arrBestMatches.length == 0) {
     alert("The company does not have a stock symbol");
+    // THIS IS AN ALERT, Maybe we can update it to say:
+    //$("#stockList").text("0 results found")
     return usMatches[0].symbol;
   }
 
@@ -205,15 +62,85 @@ function selectOneStock(arrBestMatches) {
   for (var i = 0; i < usMatches.length; i++) {
     var bStk = $("<button>")
       .attr("type", "submit")
-      .text(usMatches[i].symbol + " " + usMatches[i].name)
+      .attr("name", usMatches[i].name)
       .attr("id", usMatches[i].symbol)
-      .click(function () {
-        alert("Symbol " + this.id + " has been selected");
-        return this.id;
+      .text(usMatches[i].symbol + " " + usMatches[i].name)
+      .click(function generatePage () {
+        currentSymbol = this.id;        
+        currentCompany = this.name;         
+        weeklyDate = [];
+        weeklyValue = [];
+        buildCompanyInfo();
+        buildStockInfo();
+        buildNewsInfo()
+        // alert("Symbol " + this.id + " has been selected");
+        // return this.id;
       });
     $("#stockList").append(bStk);
   }
-}
+}   //end of select one stock function, then updates unverisal variable with symbol
+}); //end of api call that decides our comany
+}); // end of function that occurs when user clicks search
+
+        //===========END OF PART I ======================================\\
+        //==========PART II=== Company INFO=========\\
+function buildCompanyInfo() {
+
+
+        // new url that takes the comany as a symbol not user input
+        var companySearch =
+        "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
+        currentSymbol +
+        "&apikey=" +
+        vantageKey;
+    
+          //API call to use symbol to call company info
+      $.ajax({
+        url: companySearch,
+        method: "GET",
+      }).then(function (response) {
+          return response;
+
+
+      });
+              // END of UseSycompany info API
+
+  
+} //end of function that build company info
+
+        //===========END OF PART II==========================\\
+        //===========PART III===Stock Performance=============\\
+function buildStockInfo() {
+
+          // URL to call API for the STOCK INFO
+          var stockUrl =
+          "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" +
+          currentSymbol +
+          "&apikey=" +
+          vantageKey;
+            // API CALL for the STOCK INFO in order to build chart and display high low etc
+        $.ajax({
+          url: stockUrl,
+          method: "GET",
+        }).then(function (response) {
+
+      
+
+
+// These don't yet exist on the page, but we can add easily
+// High and low price in the last year
+$("#high").text(high);
+var testArray = [1, 6, 8, 9, 9];
+var high =
+  "$" +
+  testArray.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+var low =
+  "$" +
+  testArray.reduce(function (a, b) {
+    return Math.min(a, b);
+  });
 
 //price has gone up or down since last week, color change as well
 var testCurrentDay = 75.31;
@@ -230,4 +157,112 @@ if (changeStock > 0) {
 } else {
   $("#percent").attr("class", "black");
 }
-console.log();
+
+
+
+
+
+              //Assembles the data that is inserted into the chart
+          for (let i = 0; i < 52; i++) {
+            var priceDate = Object.keys(response["Weekly Time Series"])[i];
+            // console.log(response["Weekly Time Series"][priceDate]["4. close"]);
+            weeklyValue.unshift(
+              response["Weekly Time Series"][priceDate]["4. close"]
+            );
+            weeklyDate.unshift(priceDate);
+          }
+          visualData();
+      
+              //function the builds the chart into the page on the canvas element
+                  //source: chartjs.org  
+          function visualData() {
+            var ctx = document.getElementById("chart").getContext("2d");
+            Chart.defaults.global.defaultFontColor = 'black';
+            var myChart = new Chart(ctx, {
+              type: "line",
+              data: {
+                labels: weeklyDate, //what is listed as the x axis value
+                datasets: [
+                  {
+                    label: currentCompany,
+                    data: weeklyValue,
+                    backgroundColor: "rgba(0, 0, 0, 1)",
+                    borderColor: "rgba(0, 0, 0, 1)",
+                    borderWidth: 3,
+                    fill: false,
+                  }
+                ],
+              },
+              options: {
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        // beginAtZero: true
+                        callback: function (value, index, values) {
+                          return "$" + value;
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            });
+          }  //End of the stuff the builds the chart on the canvas element
+        }); //END of STOCK API
+  } //end of build page function
+  
+
+        //==========END OF PART III=================\\
+        //==========PART IV======NEWS===========\\
+function buildNewsInfo() {
+                //URL for calling the news API
+          var newsCompany =
+            "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+            currentCompany +
+            "&api-key=" + 
+            newsApiKey;
+
+        $.ajax({
+          url: newsCompany,
+          method: "GET",
+        }).then(function (response) {
+            // return response;
+            var headLine = response.response.docs[0].headline.main;
+            var snippet = response.response.docs[0].snippet;
+            // var newsUrl = response.response.docs[0].web_url;
+            newsAppended();
+            function newsAppended() {
+              $("#newsHeadLine").text(headLine);
+              $("#newsSnippet").text(snippet);
+              console.log(headLine);
+            }
+
+            
+        })  //end of NEWS API stuff
+}// end of buildNewsInfo
+
+
+        //==========END OF PART IV=================\\
+
+
+
+
+
+
+
+
+    //    Local storage stuff
+    var stockSymbol = "tsla";
+    localStorage.setItem("key1", stockSymbol);
+
+    var storedInfo = localStorage.getItem("key1");
+    function displayLastSearch() {
+      if (storedInfo !== undefined && storedInfo !== null) {
+        stockSymbol = storedInfo;
+      }
+    }
+
+
+
+
